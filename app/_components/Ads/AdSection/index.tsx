01e62849a6d5e8b103aade1ce42dd965f63e6f38
Hur Abbas <hurabbas.dev@gmail.com>
2024-08-28 10:26:39 -0400

    fixes add section

--- app/_components/Ads/AdSection/index.tsx
+++ app/_components/Ads/AdSection/index.tsx
@@ -1,6 +1,6 @@
 function AdSection() {
   return (
-    <div className='my-5 flex h-12 justify-center bg-neutral-300 py-3 text-center text-xl'>
+    <div className='my-5 flex h-[100px] justify-center bg-gray-200 py-3 text-center text-sm text-gray-400'>
       Ad Section
     </div>
   );
