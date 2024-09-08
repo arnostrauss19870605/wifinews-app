03ff0a9d3bdd36308e13f97f8e2f4627e444310c
Hur Abbas <hurabbas.dev@gmail.com>
2024-09-08 15:29:07 -0400

    adds ads slot to /home page

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,26 +1,137 @@
 'use client';
-import React from 'react';
-import ConnectWiFi from '@/app/_components/ConnectWiFi';
+import Script from 'next/script';
 import LearningMaterial from '@/app/_components/LearningMaterial';
 import DiscussionForum from '@/app/_components/DiscussionForum';
 import News from '@/app/(pages)/news/page';
+import ConnectWiFi from '@/app/_components/ConnectWiFi';
 
 function Home() {
   return (
-    <>
+    <div>
+      {/* GPT Configuration and Ad Display */}
+      <Script id='gpt-home-setup' strategy='afterInteractive'>
+        {`
+          window.googletag = window.googletag || { cmd: [] };
+
+          const queryValues = window.location.search;
+          const urlParams = new URLSearchParams(queryValues);
+          let utm_medium = 'NULL';
+
+          if (urlParams.has('utm_medium')) {
+            utm_medium = urlParams.get('utm_medium') || 'NULL';
+            console.log('Utm Medium exists as:', utm_medium);
+          } else {
+            console.log('Utm Medium does not exist, value to be populated:', utm_medium);
+          }
+
+          window.googletag.cmd.push(function () {
+            // Define size mappings
+            const mapping1 = googletag.sizeMapping()
+              .addSize([1400, 0], [[728, 90], 'fluid'])
+              .addSize([1200, 0], [[728, 90], 'fluid'])
+              .addSize([1000, 0], [[728, 90], 'fluid'])
+              .addSize([700, 0], [[468, 60], [320, 50], [300, 50], 'fluid', [300, 250], [320, 100], [300, 100]])
+              .addSize([600, 0], [[468, 60], [320, 50], [300, 50], 'fluid', [300, 100], [320, 100], [300, 250]])
+              .addSize([400, 0], [[320, 50], [300, 50], 'fluid', [320, 100], [300, 250], [300, 100]])
+              .addSize([300, 0], [[320, 50], [300, 250], [320, 100], [300, 50], [300, 100], 'fluid'])
+              .build();
+
+            const mapping3 = googletag.sizeMapping()
+              .addSize([1400, 0], ['fluid', [728, 90], [300, 250], [300, 600], [468, 60]])
+              .addSize([1200, 0], ['fluid', [728, 90], [468, 60], [300, 250], [300, 600]])
+              .addSize([1000, 0], ['fluid', [728, 90], [468, 60], [300, 250], [300, 600]])
+              .addSize([700, 0], ['fluid', [468, 60], [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
+              .addSize([600, 0], ['fluid', [468, 60], [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
+              .addSize([400, 0], ['fluid', [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
+              .addSize([300, 0], ['fluid', [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
+              .build();
+
+            const mapping4 = googletag.sizeMapping()
+              .addSize([1400, 0], [[728, 90], 'fluid'])
+              .addSize([1200, 0], [[728, 90], 'fluid'])
+              .addSize([1000, 0], [[728, 90], 'fluid'])
+              .addSize([700, 0], ['fluid', [468, 60], [320, 50], [300, 50], [320, 100], [300, 100], [300, 250]])
+              .addSize([600, 0], ['fluid', [468, 60], [320, 50], [300, 50], [320, 100], [300, 100], [300, 250]])
+              .addSize([400, 0], ['fluid', [320, 50], [300, 50], [320, 100], [300, 100], [300, 250]])
+              .addSize([300, 0], ['fluid', [320, 50], [300, 50], [320, 100], [300, 100], [300, 250]])
+              .build();
+
+            // Define ad slots and display them
+            googletag.defineSlot('/22047902240/wifinews/homepage_top_leaderboard', ['fluid', [320, 100], [320, 50], [300, 250], [468, 60], [728, 90]], 'div-gpt-ad-6641866-1')
+              .defineSizeMapping(mapping1)
+              .addService(googletag.pubads());
+
+            googletag.defineSlot('/22047902240/wifinews/homepage_mpu_hpa', ['fluid', [300, 250], [300, 600], [320, 50], [320, 100], [468, 60], [728, 90]], 'div-gpt-ad-6641866-2')
+              .defineSizeMapping(mapping3)
+              .addService(googletag.pubads());
+
+            googletag.defineSlot('/22047902240/wifinews/homepage_mpu_hpa_2', ['fluid', [300, 250], [300, 600], [320, 50], [320, 100], [468, 60], [728, 90]], 'div-gpt-ad-6641866-3')
+              .defineSizeMapping(mapping3)
+              .addService(googletag.pubads());
+
+            googletag.defineSlot('/22047902240/wifinews/Homepage_bottom_1', ['fluid', [300, 250], [320, 100], [320, 50], [468, 60], [728, 90]], 'div-gpt-ad-6641866-4')
+              .defineSizeMapping(mapping1)
+              .addService(googletag.pubads());
+
+            googletag.defineSlot('/22047902240/wifinews/Homepage_bottom_2', ['fluid', [300, 250], [320, 50], [320, 100], [468, 60], [728, 90]], 'div-gpt-ad-6641866-5')
+              .defineSizeMapping(mapping1)
+              .addService(googletag.pubads());
+
+            googletag.defineSlot('/22047902240/wifinews/homepage_sticky', ['fluid', [320, 50], [320, 100], [300, 250], [468, 60], [728, 90]], 'div-gpt-ad-6641866-6')
+              .defineSizeMapping(mapping4)
+              .addService(googletag.pubads());
+
+            // Enable services and set targeting
+            googletag.pubads().enableSingleRequest();
+            googletag.pubads().setTargeting('Medium', [utm_medium]);
+            googletag.pubads().collapseEmptyDivs();
+            googletag.pubads().setCentering(true);
+            googletag.enableServices();
+
+            // Display the ad slots
+            googletag.display('div-gpt-ad-6641866-1');
+            googletag.display('div-gpt-ad-6641866-2');
+            googletag.display('div-gpt-ad-6641866-3');
+            googletag.display('div-gpt-ad-6641866-4');
+            googletag.display('div-gpt-ad-6641866-5');
+            googletag.display('div-gpt-ad-6641866-6');
+          });
+        `}
+      </Script>
+
       <ConnectWiFi
         step={2}
         redirectUrl='/interstitial'
-        timerDuration={10}
+        timerDuration={16}
         showButtonAt={6}
         showButton={true}
       />
+
+      <div className='my-4 flex w-full items-center justify-center'>
+        <div id='div-gpt-ad-6641866-1'></div>
+      </div>
+
       <LearningMaterial />
       <DiscussionForum />
       <div className='my-10'>
         <News />
       </div>
-    </>
+      <div className='my-4 flex w-full items-center justify-center'>
+        <div id='div-gpt-ad-6641866-2'></div>
+      </div>
+      <div className='my-4 flex w-full items-center justify-center'>
+        <div id='div-gpt-ad-6641866-3'></div>
+      </div>
+      <div className='my-4 flex w-full items-center justify-center'>
+        <div id='div-gpt-ad-6641866-4'></div>
+      </div>
+      <div className='my-4 flex w-full items-center justify-center'>
+        <div id='div-gpt-ad-6641866-5'></div>
+      </div>
+      <div className='my-4 flex w-full items-center justify-center'>
+        <div id='div-gpt-ad-6641866-6'></div>
+      </div>
+    </div>
   );
 }
 
