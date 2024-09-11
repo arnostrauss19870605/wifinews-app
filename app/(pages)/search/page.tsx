d80c1c28b5594654b3b636eb9f0f343e1929c85d
Hur Abbas <hurabbas.dev@gmail.com>
2024-09-11 08:40:50 -0400

    replace search page ad slot tags

--- app/(pages)/search/page.tsx
+++ app/(pages)/search/page.tsx
@@ -1,21 +1,18 @@
 'use client';
 import Script from 'next/script';
-import LearningMaterial from '@/app/_components/LearningMaterial';
-import DiscussionForum from '@/app/_components/DiscussionForum';
-import News from '@/app/(pages)/news/page';
 import { getUtmParams } from '@/app/_utils/utm.util';
 
 function Search() {
   return (
     <div>
       {/* GPT Configuration and Ad Display */}
-      <Script id='gpt-home-setup' strategy='afterInteractive'>
+      <Script id='gpt-search-setup' strategy='afterInteractive'>
         {`
           window.googletag = window.googletag || { cmd: [] };
 
           window.googletag.cmd.push(function () {
             const utmParams = ${JSON.stringify(getUtmParams())};
-            console.log("homepage utm params =>",utmParams);
+            console.log("search page utm params =>", utmParams);
             Object.entries(utmParams).forEach(([key, value]) => {
               googletag.pubads().setTargeting(key, value);
             });
@@ -31,16 +28,6 @@ function Search() {
               .addSize([300, 0], [[320, 50], [300, 250], [320, 100], [300, 50], [300, 100], 'fluid'])
               .build();
 
-            const mapping3 = googletag.sizeMapping()
-              .addSize([1400, 0], ['fluid', [728, 90], [300, 250], [300, 600], [468, 60]])
-              .addSize([1200, 0], ['fluid', [728, 90], [468, 60], [300, 250], [300, 600]])
-              .addSize([1000, 0], ['fluid', [728, 90], [468, 60], [300, 250], [300, 600]])
-              .addSize([700, 0], ['fluid', [468, 60], [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
-              .addSize([600, 0], ['fluid', [468, 60], [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
-              .addSize([400, 0], ['fluid', [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
-              .addSize([300, 0], ['fluid', [320, 50], [300, 50], [320, 100], [300, 100], [300, 250], [300, 600]])
-              .build();
-
             const mapping4 = googletag.sizeMapping()
               .addSize([1400, 0], [[728, 90], 'fluid'])
               .addSize([1200, 0], [[728, 90], 'fluid'])
@@ -52,27 +39,11 @@ function Search() {
               .build();
 
             // Define ad slots and display them
-            googletag.defineSlot('/22047902240/wifinews/homepage_top_leaderboard', ['fluid', [320, 100], [320, 50], [300, 250], [468, 60], [728, 90]], 'div-gpt-ad-6641866-1')
+            googletag.defineSlot('/22047902240/wifinews/search_top320x50', ['fluid', [300, 250], [320, 50], [320, 100], [468, 60], [728, 90]], 'div-gpt-ad-2121612-1')
               .defineSizeMapping(mapping1)
               .addService(googletag.pubads());
 
-            googletag.defineSlot('/22047902240/wifinews/homepage_mpu_hpa', ['fluid', [300, 250], [300, 600], [320, 50], [320, 100], [468, 60], [728, 90]], 'div-gpt-ad-6641866-2')
-              .defineSizeMapping(mapping3)
-              .addService(googletag.pubads());
-
-            googletag.defineSlot('/22047902240/wifinews/homepage_mpu_hpa_2', ['fluid', [300, 250], [300, 600], [320, 50], [320, 100], [468, 60], [728, 90]], 'div-gpt-ad-6641866-3')
-              .defineSizeMapping(mapping3)
-              .addService(googletag.pubads());
-
-            googletag.defineSlot('/22047902240/wifinews/Homepage_bottom_1', ['fluid', [300, 250], [320, 100], [320, 50], [468, 60], [728, 90]], 'div-gpt-ad-6641866-4')
-              .defineSizeMapping(mapping1)
-              .addService(googletag.pubads());
-
-            googletag.defineSlot('/22047902240/wifinews/Homepage_bottom_2', ['fluid', [300, 250], [320, 50], [320, 100], [468, 60], [728, 90]], 'div-gpt-ad-6641866-5')
-              .defineSizeMapping(mapping1)
-              .addService(googletag.pubads());
-
-            googletag.defineSlot('/22047902240/wifinews/homepage_sticky', ['fluid', [320, 50], [320, 100], [300, 250], [468, 60], [728, 90]], 'div-gpt-ad-6641866-6')
+            googletag.defineSlot('/22047902240/wifinews/search_sticky', ['fluid', [320, 50], [320, 100], [300, 250], [468, 60], [728, 90]], 'div-gpt-ad-2121612-2')
               .defineSizeMapping(mapping4)
               .addService(googletag.pubads());
 
@@ -83,36 +54,20 @@ function Search() {
             googletag.enableServices();
 
             // Display the ad slots
-            googletag.display('div-gpt-ad-6641866-1');
-            googletag.display('div-gpt-ad-6641866-2');
-            googletag.display('div-gpt-ad-6641866-3');
-            googletag.display('div-gpt-ad-6641866-4');
-            googletag.display('div-gpt-ad-6641866-5');
-            googletag.display('div-gpt-ad-6641866-6');
+            googletag.display('div-gpt-ad-2121612-1');
+            googletag.display('div-gpt-ad-2121612-2');
           });
         `}
       </Script>
 
       <div className='my-4 flex w-full items-center justify-center'>
-        <div id='div-gpt-ad-6641866-1'></div>
+        <div id='div-gpt-ad-2121612-1'></div>
       </div>
 
       <div>Search...</div>
 
       <div className='my-4 flex w-full items-center justify-center'>
-        <div id='div-gpt-ad-6641866-2'></div>
-      </div>
-      <div className='my-4 flex w-full items-center justify-center'>
-        <div id='div-gpt-ad-6641866-3'></div>
-      </div>
-      <div className='my-4 flex w-full items-center justify-center'>
-        <div id='div-gpt-ad-6641866-4'></div>
-      </div>
-      <div className='my-4 flex w-full items-center justify-center'>
-        <div id='div-gpt-ad-6641866-5'></div>
-      </div>
-      <div className='my-4 flex w-full items-center justify-center'>
-        <div id='div-gpt-ad-6641866-6'></div>
+        <div id='div-gpt-ad-2121612-2'></div>
       </div>
     </div>
   );
