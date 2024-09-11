7d36debb58b706969d12c4e59ea3a97250214779
Hur Abbas <hurabbas.dev@gmail.com>
2024-09-11 06:25:37 -0400

    refactor cancel page to handle /forti_2 page

--- app/(pages)/cancel/page.tsx
+++ app/(pages)/cancel/page.tsx
@@ -4,7 +4,8 @@ import NoThankYouClient from '@/app/_components/NoThankYouClient';
 export default function NoThankYou() {
   const headersList = headers();
   const referer = headersList.get('referer') || '';
-  const isFromInterstitial = referer.includes('/interstitial');
+  const isFromInterstitial =
+    referer.includes('/interstitial') || referer.includes('/forti_2');
 
   console.log('Server-side Referer:', referer);
   console.log('Server-side Is from interstitial:', isFromInterstitial);
