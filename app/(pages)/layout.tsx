96de62a4486fc8bcc36e178fb8bea76191484fba
Hur Abbas <hurabbas.dev@gmail.com>
2024-10-07 05:26:41 -0400

    adds global this import

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -29,6 +29,11 @@ export default function RootLayout({
           src='https://securepubads.g.doubleclick.net/tag/js/gpt.js'
           strategy='beforeInteractive'
         />
+        <script
+          src='https://unpkg.com/@ungap/global-this@0.4.4/min.js'
+          noModule
+          async
+        ></script>
       </head>
       <body className={inter.className}>
         <AuthProvider>
