b107230f37c5824e170098af210a9831ae5795d9
Hur Abbas <hurabbas.dev@gmail.com>
2024-10-14 08:09:28 -0400

    improves timer logic on /home route

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Script from 'next/script';
 import LearningMaterial from '@/app/_components/LearningMaterial';
 import DiscussionForum from '@/app/_components/DiscussionForum';
@@ -9,30 +9,30 @@ import { getUtmParams, appendUtmParams } from '@/app/_utils/utm.util';
 import { FaWifi } from 'react-icons/fa';
 
 function Home() {
-  const [timer, setTimer] = useState(20);
+  const [timer, setTimer] = useState(16);
   const [isButtonVisible, setButtonVisible] = useState(false);
 
-  useEffect(() => {
-    const countdownInterval = setInterval(() => {
-      setTimer((prevTimer) => {
-        if (prevTimer > 0) {
-          const newTime = prevTimer - 1;
-          if (newTime <= 10) {
-            setButtonVisible(true);
-          }
-          return newTime;
+  const countdown = useCallback(() => {
+    setTimer((prevTimer) => {
+      if (prevTimer > 0) {
+        const newTime = prevTimer - 1;
+        if (newTime <= 10) {
+          setButtonVisible(true);
         }
-        return 0;
-      });
-    }, 1000);
+        return newTime;
+      }
+      return 0;
+    });
+  }, []);
 
-    if (timer === 0) {
-      clearInterval(countdownInterval);
+  useEffect(() => {
+    if (timer > 0) {
+      const timeoutId = setTimeout(countdown, 1000);
+      return () => clearTimeout(timeoutId);
+    } else {
       window.location.href = appendUtmParams('/interstitial');
     }
-
-    return () => clearInterval(countdownInterval);
-  }, [timer]);
+  }, [timer, countdown]);
 
   return (
     <div>
