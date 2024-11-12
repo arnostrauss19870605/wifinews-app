b6a37e7b385c54f328b16eec2096343d4752d89f
Hur Abbas <hurabbas.dev@gmail.com>
2024-11-12 05:05:33 -0500

    updates learn copy

--- app/_components/LearningMaterial/index.tsx
+++ app/_components/LearningMaterial/index.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link';
 function LearningMaterial() {
   return (
     <div className='my-4 w-full rounded-md bg-gray-100 px-3 py-4 text-center shadow-md'>
-      <h2 className='mb-2 text-xl font-semibold'>Access Learning Material</h2>
+      <h2 className='mb-2 text-xl font-semibold'>
+        Access Free Learning Material
+      </h2>
       <p className='mb-3 text-sm'>
-        Discover a wide range of learning resources to help you stay informed
-        and educated.
+        Explore a variety of free courses to gain valuable knowledge and skills
+        at no cost.
       </p>
       <div className='flex justify-center'>
         <Link href='/learn'>
@@ -15,7 +17,7 @@ function LearningMaterial() {
             type='button'
             className='flex items-center rounded-md bg-slate-900 px-3 py-2 text-sm font-medium text-white focus:outline-none'
           >
-            <FaBook className='mr-1' /> Learn Now
+            <FaBook className='mr-1' /> Start Learning for Free
           </button>
         </Link>
       </div>
