85b63d19846e55eb8e94e9b48da9c3e58f72bc3c
Hur Abbas <hurabbas.dev@gmail.com>
2025-03-16 12:53:42 -0400

    monetize more v14

--- app/_components/AdWrapper/index.tsx
+++ app/_components/AdWrapper/index.tsx
@@ -1,16 +1,45 @@
 'use client';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState, useRef } from 'react';
 
 interface AdWrapperProps {
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 const AdWrapper: React.FC<AdWrapperProps> = ({ children }) => {
+  const [isAdLoaded, setIsAdLoaded] = useState(false);
+  const adContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!adContainerRef.current) return;
+    const checkAdContent = () => {
+      if (adContainerRef.current?.childNodes.length) {
+        setIsAdLoaded(true);
+      } else {
+        setIsAdLoaded(false);
+      }
+    };
+
+    const observer = new MutationObserver(() => {
+      checkAdContent();
+    });
+
+    observer.observe(adContainerRef.current, {
+      childList: true,
+      subtree: true,
+    });
+
+    checkAdContent();
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <div className='relative mt-1 inline-block border'>
-      <div className='absolute left-0 top-0 rounded-bl-[4px] bg-white/80 px-[6px] py-[2px] text-[12px] font-thin uppercase text-gray-800'>
-        Advertisement
-      </div>
+    <div className='relative mt-1 inline-block border' ref={adContainerRef}>
+      {isAdLoaded && (
+        <div className='absolute left-0 top-0 rounded-bl-[4px] bg-white/80 px-[6px] py-[2px] text-[12px] font-thin uppercase text-gray-800'>
+          Advertisement
+        </div>
+      )}
       {children}
     </div>
   );
